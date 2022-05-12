a6a593d7094454e707ee31db94f2e79c862446ba
Naiara Saratxaga <99652446+NaiSaratxaga@users.noreply.github.com>
2022-05-12 12:59:03 +0200

    node modules

--- src/services/Api.js
+++
@@ -1,8 +0,0 @@
-const dataApi = (data) => {
-  return fetch('https://awesome-profile-cards.herokuapp.com/card/', {
-    method: 'POST',
-    body: JSON.stringify(data),
-    headers: { 'Content-Type': 'application/json' },
-  }).then((response) => response.json());
-};
-export default dataApi;
---
+++ src/src/index.js
@@ -0,0 +1,27 @@
+// Fichero src/index.js
+
+// Importamos los dos módulos de NPM necesarios para trabajar
+const express = require('express');
+const cors = require('cors');
+
+// Creamos el servidor
+const server = express();
+
+// Configuramos el servidor
+server.use(cors());
+server.use(express.json());
+
+// Arrancamos el servidor en el puerto 3000
+const serverPort = 4000;
+server.listen(serverPort, () => {
+  console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+// Escribimos los endpoints que queramos
+server.post('/card', (req, res) => {
+  const response = {
+    success: true,
+    cardURL: 'ejemplo de ruta',
+  };
+  res.json(response);
+});
